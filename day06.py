df1ae47b222f9820a55db8ffc43ddac29fab9115
chris <chall168421@gmail.com>
2023-03-14 12:13:45 +0700

    day 6 complete

--- day06.py
+++ day06.py
@@ -25,68 +25,28 @@ def solve(d):
     [[safe.extend([(x, y)]) for x in range(w)] for y in range(h)]
     safe = set(safe)
 
-    dangerous = set()
 
-    skip = False
-    
     for roid in d:
         x_origin, y_origin, x_vel, y_vel = map(int, roid.split())
 
-        #print(x_origin, y_origin, x_vel, y_vel)
+        x = x_origin + (3600 * x_vel)
+        y = y_origin + (3600 * y_vel)
+        if (x, y) in safe:
+            safe.remove((x, y))
 
-        if x_vel == 0 == y_vel:
-            skip = True
-        elif x_origin > 99 and x_vel >= 0:
-            skip = True
-        elif y_origin > 99 and y_vel >= 0:
-            skip = True
-        elif x_origin < 0 and x_vel <= 0:
-            skip = True
-        elif y_origin < 0 and y_vel <= 0:
-            skip = True
-        if skip:
+        for i in range(60):
+            x += x_vel
+            y += y_vel
 
-            continue
+            if (x, y) in safe:
+                safe.remove((x, y))
 
-        x_path, y_path = [x_origin], [y_origin]
+    return list(safe)
 
-        
-        max_ = 5000
-        min_ = -5000       
+    
+    
 
-        if x_vel > 0:
-            x_path = list(range(x_origin, max_, x_vel))
-        elif x_vel < 0:
-            x_path = list(range(x_origin, min_, x_vel))
-        if y_vel > 0:
-            y_path = list(range(y_origin, max_, y_vel))
-        elif y_vel < 0:
-            y_path = list(range(y_origin, min_, y_vel))
-            
-        diff = len(x_path) - len(y_path)
-
-        ## ensure zip will work - one path can't be shorter else will truncate
-
-        if diff > 0:
-            if y_vel == 0:
-                y_path = y_path + [y_origin for _ in range(abs(diff))]
-            else:
-                limit = max_ if y_vel > 0 else min_
-                y_path = y_path + list(range(y_path[-1]+1, limit, y_vel))
-        elif diff < 0:
-            if x_vel == 0:
-                x_path = x_path + [x_origin for _ in range(abs(diff))]
-            else:
-                limit = max_ if y_vel > 0 else min_
-                x_path = x_path + list(range(x_path[-1]+1, limit,  x_vel))
-
-
-        path = zip(x_path, y_path)
-
-        [dangerous.add((x, y)) for x, y in path if 0<=x<w and 0<=y<h]
-        print(len(dangerous))
-
-    return safe.difference(dangerous)
+       
 
 #w, h = 8, 8
 #result = solve(load("test"))
